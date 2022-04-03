93b67191aaa30758154e01ef84d6d097171534b6
Joshna-Mutchakarla <joshnaramana2000@gmail.com>
2022-04-03 17:50:06 +0530

    added implementation

---
+++ atm.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+void mainMenu()
+ {
+    printf("******************Hello!*******************\n");
+    printf("**********Welcome to ATM Banking***********\n\n");
+    printf("****Please choose one of the options below****\n\n");
+    printf("&lt; 1 &gt;  Check Balance\n");
+    printf("&lt; 2 &gt;  Deposit\n");
+    printf("&lt; 3 &gt;  Withdraw\n");
+    printf("&lt; 4 &gt;  Exit\n\n");
+ 
+}
